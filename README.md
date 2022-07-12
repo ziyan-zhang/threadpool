66bc179a45073356794c454136c41c51dcc123ac
ziyanzhang <1920548152@qq.com>
2022-07-12 15:10:54 +0800

    路径整洁

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # A Simple Thread Pool Impletemeted in C++
 
-threadpoll：53, this->stop和this->tasks.empty()将情况分为了四种。
--- thread_pool.h
+++ thread_pool.h
@@ -10,13 +10,14 @@
 #include <stdexcept>
 #include <thread>
 #include <vector>
+#include <string>
+#include <iostream>
 
 class ThreadPool {
  public:
   ThreadPool(size_t);
   template <class F, class... Args>
-  auto enqueue(F&& f, Args&&... args)
-      -> std::future<typename std::result_of<F(Args...)>::type>;
+  auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
   ~ThreadPool();
 
  private:
@@ -35,30 +36,34 @@ class ThreadPool {
  */
 inline ThreadPool::ThreadPool(size_t pool_size) : stop(false) {
   // 建立pool_size个线程，每个线程的任务是从任务队列中拉出一个任务并执行
-  for (size_t i = 0; i < pool_size; ++i)
-    workers.emplace_back([this] {
-      while (true) {
-        std::function<void()> task;
-        {
-          std::unique_lock<std::mutex> lock(this->queue_mutex);
-          // [条件变量]等待主线程设置stop或者任务队列不空
-          // 下面这行的语法，std::condition_variable::wait()第二种用法
-          // template<class Predicate>
-          // void wait (unique_lock<mutex>& lck, Predicate pred)
-          // 在这种情况下（即设置了Predicate），只有当pred条件为false时调用wait()才会阻塞当前线程，
-          // 并且在收到其他线程的通知后只有当pred为true时才会被解除阻塞。
-          // 类似于 while(!pred()) wait(lck);
-          this->condition.wait(
-              lock, [this] { return this->stop || !this->tasks.empty(); });
-          // 这里this->stop和任务队列是否为空将情况分为了四种。可以画图自己看一下。
-          if (this->stop && this->tasks.empty()) return;  // 主线程设置停止执行，同时任务队列为空
-          // 任务队列不为空，将队首的任务拿出来执行
-          task = std::move(this->tasks.front());
-          this->tasks.pop();
-        }
-        task();  // 执行任务
-      }
-    });
+  for (size_t i = 0; i < pool_size; ++i) {
+      workers.emplace_back([this] {
+          // 下面的 while (true) 后面的整段代码应该是作为一个thread初始化的函数，立即被执行的。
+          while (true) {
+              std::function<void()> task;
+              {
+                  std::unique_lock<std::mutex> lock(this->queue_mutex);
+                  // template <class Predicate>，void wait (unique_lock<mutex>& lck, Predicate pred) = while(!pred()) wait(lck);
+                  // 没拿到队列锁lck、并且pred为false时才阻塞；收到其他线程通知后（从wait走出来，拿到锁lck了），再次检查只有当pred为true时才解除阻塞。
+                  this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                  // pred为true: 拿到stop信号或者队列非空; pred为false: 没拿到stop信号、并且队列为空。
+                  // 后者意味着：没拿到stop信号的大前提下：刚初始化线程池，故线程队列为空; 或者线程队列中的任务全被做完了，故线程队列变空。两种情况都会阻塞。
+
+                  // 没有被阻塞的情况，代码走到这里。要么拿到了stop信号，要么队列不空。分为如下三种情况：
+                  // 1. 拿到stop信号、队列为空：停止while，也即这个线程不再是永生（while (true)）的了，要结束了。
+                  if (this->stop && this->tasks.empty()) return;  // 主线程设置停止执行，同时任务队列为空
+                  // 2. 3. 任务队列不空：那就把队列中的任务执行完：将队首任务拿出来执行。
+                  task = std::move(this->tasks.front());
+                  this->tasks.pop();
+              }
+              // 选出task之后，队列就不用被保护了。所以可以先交出队列锁，再执行任务。否则，就只剩线程安全，没有并发了。
+              task();
+              // 每完成一个任务，这个地方就通过一次。也即完成了多少个任务，task()执行多少次，这里就通过多少次。
+          }
+      });
+      std::cerr << "line 61" << std::endl;
+      // 这个位置在任何任务被加进队列前(初始化线程池的时候，并没有办法同时或在此之前把任务加进去)，通过了pool_size次，并且只运行pool_size次。
+  }
 }
 
 inline ThreadPool::~ThreadPool() {
@@ -75,21 +80,20 @@ inline ThreadPool::~ThreadPool() {
 
 // 添加一个新任务，其函数类型为F，参数类型为Args，这里的具名右值引用是为了统一const
 // T &和T &两种形参，算是形参的万能模板
+// std::result_of，用于在编译的时候推导出一个可调用对象（函数，std::function或者重载了operator()操作的对象等）的返回值类型。主要用于模板编写中。
 template <class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
     -> std::future<typename std::result_of<F(Args...)>::type> {
   // std::result_of<>::type是函数的返回值类型
+//  auto aa = std::result_of<F(Args...)>;
   using return_type = typename std::result_of<F(Args...)>::type;
+  // std::make_shared，c++11，返回一个智能指针。
+  // std::packaged_task，包装一个可调用的对象，并且允许一步获取该可调用对象产生的结果。与std::function类似，
+  // 只不过将其包装的可调用对象的执行结果传递给一个std::future对象（该对象通常在另一个线程中获取std::packaged_task的执行结果）。
+  // std::bind头文件是<functional>，它是一个函数适配器，接受一个可调用对象，生成一个新的可调用对象来“适应”原对象的参数列表。
+  // std::forward，完美转发，使万能引用内部的形参保留原来的左右值属性。注意到f和args都被完美转发了。
   auto task_ptr = std::make_shared<std::packaged_task<return_type()>>(
       std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-
-  // make_shared，一个智能指针的模板，相比于用new初始化，提升性能，并且是异常安全的（具体先不管）
-  // 原版new初始化   std::shared_ptr<Widget> spw(new Widget);
-  // 新版std::make_shared来替换  auto spw = std::make_shared<Widget>();
-
-  // std::bind的头文件时<functional>，它是一个函数适配器，接受一个可调用对象(callable object)，
-  // 生成一个新的可调用对象来“适应”原对象的参数列表。
-
   // 分离返回值和函数体
   std::future<return_type> res = task_ptr->get_future();
   {
@@ -99,6 +103,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
     }
     // 这里*task就是std::packaged_task<return_type()>, (*task)() = f(args)，
     // 而tasks是std::functional，封装了这段执行的代码，真正的执行是在消费者线程中
+    // 下面这个是lambda表达式，capture子句[]捕获了task_ptr，参数列表()为空，可变规范、异常规范、后续返回类型为空。
     tasks.emplace([task_ptr]() { (*task_ptr)(); });
   }
   // 通知一个消费者线程，有新任务了
